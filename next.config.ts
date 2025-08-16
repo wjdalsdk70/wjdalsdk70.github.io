0d5b7a7e2e8aec4cb680212f8496c17d637ede9e
JM_DEV <kyungheeyj45@ajou.ac.kr>
2025-08-17 07:21:52 +0900

    fix: 오류 해결

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-import withMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   output: "export",
   images: { unoptimized: true },
 };
 
-export default withMDX()(nextConfig);
+export default nextConfig;
